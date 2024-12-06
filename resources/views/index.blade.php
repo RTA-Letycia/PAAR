c7c02eea1b1c3ee8ad27a444daa573f4890a98ec
RTA-LETYCIA <letycia.souza@rta.eng.br>
2024-12-06 15:58:19 -0300

    :bug: resolves responsivity of index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,16 @@
 @endsection
 @section('content')
         <!-- row inferior -->
-        <div class="row card-row align-items-center justify-content-center mb-row-sup">
-            <div class="col-4 col-lg-3">
+        <div class="row card-row align-items-center mt-row-inf">
+            <div class="col-3 col-md-4 col-lg-3 col-xl-2">
                 <div class="row d-flex justify-content-center">
-                    <div class="intern-card card col-lg-8 flex-column flex-wrap justify-content-around pb-4">
-                        <h1 class="font-rawline small align-self-center">Prazos</h1>
-                        <div class="d-flex justify-content-around text-center pl-1 align-items-center">
+                    <div class="intern-card card col-md-9 flex-column flex-wrap justify-content-around pb-4">
+                        <h1 class="font-rawline small align-self-center pb-1">Prazos</h1>
+                        <div class="d-flex justify-content-end text-center pl-1 align-items-center">
                             <h1 class="font-rawline small size-h1">Próximo ao vencimento:</h1>
                             <div class="mini-box text-white bg-teste">1</div>
                         </div>
-                        <div class="d-flex justify-content-around">
+                        <div class="d-flex justify-content-end">
                             <h1 class="font-rawline small pl-4 align-self-end">Vencidos:</h1>
                             <div class="mini-box text-white" id="blinking-element">2</div>
                         </div>
@@ -21,21 +21,21 @@
                     </div>
                 </div>
             </div>
-            <div class="col-4 col-lg-3">
+            <div class="col-3 col-md-2 col-lg-3 col-xl-2">
                 <div class="row d-flex justify-content-center">
                     <div class="intern-card intern-card-hover card col-lg-8 align-items-center justify-content-center">
-                        <h1 class="font-rawline small text-lg-center">Histórico<br>de<br>Movimentações</h1>
+                        <h1 class="font-rawline small text-center">Histórico de Movimentações</h1>
                     </div>
                 </div>
             </div>
-            <div class="col-4 col-lg-3">
+            <div class="col-3 col-md-2 col-lg-3 col-xl-2">
                 <div class="row d-flex justify-content-center">
                     <div class="intern-card intern-card-hover card col-lg-8 align-items-center justify-content-center">
                         <h1 class="font-rawline small">Lembretes</h1>
                     </div>
                 </div>
             </div>
-            <div class="col-4 col-lg-3">
+            <div class="col-3 col-md-2 col-lg-3 col-xl-2">
                 <div class="row d-flex justify-content-center">
                     <div class="intern-card intern-card-hover card col-lg-8 align-items-center justify-content-center">
                             <a class="btn d-flex align-items-center justify-content-center col-lg-12" href="{{route('process')}}" >
